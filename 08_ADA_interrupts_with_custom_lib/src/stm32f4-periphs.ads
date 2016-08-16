dd76ec5107cc1805d14fe43fa7db13fa203d8ccc
milouz <a.michelizza@gmail.com>
2016-08-16 15:58:01 +0200

    use system.address instead of localy defined type

--- 08_ADA_interrupts_with_custom_lib/src/stm32f4-periphs.ads
+++ 08_ADA_interrupts_with_custom_lib/src/stm32f4-periphs.ads
@@ -13,85 +13,85 @@ package stm32f4.periphs is
    -- Base addresses --
    --------------------
 
-   NVIC_Base      : constant address := 16#E000_E100#;
+   NVIC_Base   : constant address := system'to_address (16#E000_E100#);
 
-   GPIOD_Base     : constant address := 16#4002_0C00#; -- AHB1
-   GPIOC_Base     : constant address := 16#4002_0800#; -- AHB1
-   GPIOB_Base     : constant address := 16#4002_0400#; -- AHB1
-   GPIOA_Base     : constant address := 16#4002_0000#; -- AHB1
+   GPIOD_Base  : constant address := system'to_address (16#4002_0C00#); -- AHB1
+   GPIOC_Base  : constant address := system'to_address (16#4002_0800#); -- AHB1
+   GPIOB_Base  : constant address := system'to_address (16#4002_0400#); -- AHB1
+   GPIOA_Base  : constant address := system'to_address (16#4002_0000#); -- AHB1
 
-   RCC_Base       : constant address := 16#4002_3800#; -- AHB1
-   EXTI_Base      : constant address := 16#4001_3C00#; -- APB2
-   SYSCFG_Base    : constant address := 16#4001_3800#; -- APB2
+   RCC_Base    : constant address := system'to_address (16#4002_3800#); -- AHB1
+   EXTI_Base   : constant address := system'to_address (16#4001_3C00#); -- APB2
+   SYSCFG_Base : constant address := system'to_address (16#4001_3800#); -- APB2
 
-   USART6_Base    : constant address := 16#4001_1400#; -- APB2
-   UART5_Base     : constant address := 16#4000_5000#; -- APB1
-   UART4_Base     : constant address := 16#4000_4C00#; -- APB1
-   USART3_Base    : constant address := 16#4000_4800#; -- APB1
-   USART2_Base    : constant address := 16#4000_4400#; -- APB1
-   USART1_Base    : constant address := 16#4001_1000#; -- APB2
+   USART6_Base : constant address := system'to_address (16#4001_1400#); -- APB2
+   UART5_Base  : constant address := system'to_address (16#4000_5000#); -- APB1
+   UART4_Base  : constant address := system'to_address (16#4000_4C00#); -- APB1
+   USART3_Base : constant address := system'to_address (16#4000_4800#); -- APB1
+   USART2_Base : constant address := system'to_address (16#4000_4400#); -- APB1
+   USART1_Base : constant address := system'to_address (16#4001_1000#); -- APB2
 
    -----------------
    -- Peripherals --
    -----------------
 
    GPIOA : aliased stm32f4.gpio.t_GPIO_port
-      with import, address => system'to_address (GPIOA_Base);
+      with import, address => GPIOA_Base;
    -- Note: 'import' aspect means that the actual values are defined outside
    -- the application and should not be initialized
 
    GPIOB : aliased stm32f4.gpio.t_GPIO_port
-      with import, address => system'to_address (GPIOB_Base);
+      with import, address => GPIOB_Base;
 
    GPIOC : aliased stm32f4.gpio.t_GPIO_port
-      with import, address => system'to_address (GPIOC_Base);
+      with import, address => GPIOC_Base;
 
    GPIOD    : aliased stm32f4.gpio.t_GPIO_port
-      with import, address => system'to_address (GPIOD_Base);
+      with import, address => GPIOD_Base;
 
    RCC      : aliased stm32f4.rcc.t_RCC_periph
-      with import, address => system'to_address (RCC_Base);
+      with import, address => RCC_Base;
 
    SYSCFG   : aliased stm32f4.syscfg.t_SYSCFG_periph
-      with import, address => system'to_address (SYSCFG_Base);
+      with import, address => SYSCFG_Base;
 
    EXTI     : aliased stm32f4.exti.t_EXTI_periph
-      with import, address => system'to_address (EXTI_Base);
+      with import, address => EXTI_Base;
 
    NVIC     : aliased stm32f4.nvic.t_NVIC
-      with import, address => system'to_address (NVIC_Base);
+      with import, address => NVIC_Base;
 
    USART1   : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (USART1_Base);
+      with import, address => USART1_Base;
 
    USART2   : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (USART2_Base);
+      with import, address => USART2_Base;
 
    USART3   : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (USART3_Base);
+      with import, address => USART3_Base;
 
    UART4    : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (UART4_Base);
+      with import, address => UART4_Base;
 
    UART5    : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (UART5_Base);
+      with import, address => UART5_Base;
 
    USART6   : aliased stm32f4.usart.t_USART_periph
-      with import, address => system'to_address (USART6_Base);
+      with import, address => USART6_Base;
    
    ---------------
    -- GPIO pins --
    ---------------
 
-   PA0   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOA'access, 0);
+   PA0   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOA, 0);
 
-   PC6   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOC'access, 6);
-   PC7   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOC'access, 7);
+   PC6   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOC, 6);
+   PC7   : constant stm32f4.gpio.t_GPIO_pin  := (GPIOC, 7);
 
-   PD12  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD'access, 12);
-   PD13  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD'access, 13);
-   PD14  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD'access, 14);
-   PD15  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD'access, 15);
+   PD12  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD, 12);
+   PD13  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD, 13);
+   PD14  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD, 14);
+   PD15  : constant stm32f4.gpio.t_GPIO_pin  := (GPIOD, 15);
 
    -------------------
    -- Led & buttons --
