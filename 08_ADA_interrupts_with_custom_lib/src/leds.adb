feaec67c21fd1a935a28b0a74c43531fe694a041
milouz <a.michelizza@gmail.com>
2016-08-18 13:28:53 +0200

    Multitask (blinking leds) implementation

---
+++ 08_ADA_interrupts_with_custom_lib/src/leds.adb
@@ -0,0 +1,61 @@
+with ada.real_time; use ada.real_time;
+
+with stm32f4; use stm32f4;
+with stm32f4.gpio; use type stm32f4.gpio.t_GPIO_pin;
+with stm32f4.periphs;
+
+with buttons;  -- button that interact with those leds
+
+package body leds is
+
+   procedure init is
+   begin
+      -- The leds are on GPIOD pins. We have to enable GPIOD clock (see
+      -- RM0090, p. 65,244)
+      periphs.RCC.AHB1ENR.GPIODEN := 1;
+
+      -- Set the leds pins to output mode
+      -- (see RM0090, p. 270)
+      gpio.configure
+        (periphs.LED_GREEN,
+         gpio.MODE_OUT,
+         gpio.PUSH_PULL,
+         gpio.SPEED_HIGH,
+         gpio.PULL_DOWN);
+   
+      gpio.configure
+        (periphs.LED_RED,
+         gpio.MODE_OUT,
+         gpio.PUSH_PULL,
+         gpio.SPEED_HIGH,
+         gpio.PULL_DOWN);
+   
+      -- Led off
+      gpio.turn_off (periphs.LED_GREEN);
+      gpio.turn_off (periphs.LED_RED);
+
+      -- Init user button
+      buttons.initialize;
+   end init;
+
+   task body blinking_leds is
+      led      : gpio.t_GPIO_pin := periphs.LED_GREEN;
+      period   : constant ada.real_time.time_span := 
+         ada.real_time.milliseconds (500);
+   begin
+      init;
+      loop
+         if buttons.has_been_pressed then
+            led :=  (if led = periphs.LED_GREEN then
+                        periphs.LED_RED
+                     else
+                        periphs.LED_GREEN);
+         end if;
+         gpio.turn_on (led);
+         delay until ada.real_time.clock + period;
+         gpio.turn_off (led);
+         delay until ada.real_time.clock + period;
+      end loop;
+   end blinking_leds;
+
+end leds;
--- 08_ADA_interrupts_with_custom_lib/src/main.adb
+++ 08_ADA_interrupts_with_custom_lib/src/main.adb
@@ -1,82 +1,26 @@
-with last_chance_handler;  pragma unreferenced (last_chance_handler);
 with ada.real_time; use ada.real_time;
+with last_chance_handler;  pragma unreferenced (last_chance_handler);
 
 with stm32f4; use stm32f4;
-
 with stm32f4.gpio; use type stm32f4.gpio.t_GPIO_pin;
 with stm32f4.periphs;
 
 with serial;
-with buttons;
+with leds; pragma unreferenced (leds); -- task blinking_leds
 
 procedure main is
    counter  : integer         := 0;
-   led      : gpio.t_GPIO_pin := periphs.LED_GREEN;
    period   : constant ada.real_time.time_span := 
-      ada.real_time.milliseconds (500);
+      ada.real_time.milliseconds (1000);
 begin
 
-   --
-   -- Initialize USART (for logging purpose)
-   --
-
+   -- Initialize USART 
    serial.initialize;
-   serial.put ("Hello, world!");
-   serial.put (ASCII.CR);
-
-   --
-   -- Enable leds
-   --
-
-   -- The leds are on GPIOD pins. We have to enable GPIOD clock (see
-   -- RM0090, p. 65,244)
-   periphs.RCC.AHB1ENR.GPIODEN := 1;
-
-   -- Set the pins to output mode
-   -- (see RM0090, p. 270)
-   gpio.configure
-     (periphs.LED_GREEN,
-      gpio.MODE_OUT,
-      gpio.PUSH_PULL,
-      gpio.SPEED_HIGH,
-      gpio.PULL_DOWN);
-
-   gpio.configure
-     (periphs.LED_RED,
-      gpio.MODE_OUT,
-      gpio.PUSH_PULL,
-      gpio.SPEED_HIGH,
-      gpio.PULL_DOWN);
-
-   -- Led off
-   gpio.turn_off (periphs.LED_GREEN);
-   gpio.turn_off (periphs.LED_RED);
-
-   -- 
-   -- Init user button
-   -- 
-
-   buttons.initialize;
 
    loop
-      if buttons.has_been_pressed then
-         led :=  (if led = periphs.LED_GREEN then
-                     periphs.LED_RED
-                  else
-                     periphs.LED_GREEN);
-      end if;
-
-      gpio.turn_on (led);
-      delay until ada.real_time.clock + period;
-
-      gpio.turn_off (led);
-      delay until ada.real_time.clock + period;
-
-      serial.put
-        ("counter: " & integer'image (counter) & ASCII.CR &
-         ASCII.LF);
-
+      serial.put ("counter: " & integer'image (counter) & ASCII.CR & ASCII.LF);
       counter := counter + 1;
+      delay until ada.real_time.clock + period;
    end loop;
 
 end main;
