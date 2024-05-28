b40e349871b5a0eec682aa93f49735d92355deef
Arnauld Michelizza <arnauld.michelizza@ssi.gouv.fr>
2024-05-28 15:43:29 +0200

    update

--- 01_blinky/src/blink.adb
+++ 01_blinky/src/blink.adb
@@ -1,33 +1,35 @@
 
+------------------------------------------------------------------------------
+-- Simple blinky example for STM32F4 Discovery. Read the Readme.md file for
+-- instructions.
+------------------------------------------------------------------------------
 
--- The following packages describe some registers and peripherals common to
--- STM32F4 socs. The file i-stm32-gpio.ads, i-stm32-rcc.ads and s-stm32.ads are
--- packed with the compiler, ie.:
+
+-- The following included packages describe some registers and peripherals
+-- common to STM32F4 socs. The file i-stm32-gpio.ads, i-stm32-rcc.ads and
+-- s-stm32.ads are packed with the compiler. See:
 --   gnat_arm_elf_{version}/arm-eabi/lib/gnat/light-tasking-stm32f4/gnat/
 
-with interfaces.stm32.gpio; use interfaces.stm32.gpio;
+with interfaces.stm32.gpio;
 with interfaces.stm32.rcc;
 with system.stm32;
 
 with ada.real_time; use ada.real_time;
 
 procedure blink is
-   GREEN_LED   : constant := 12;
-   RED_LED     : constant := 14;
+   GREEN_LED   : constant := 12; -- Green led is on pin 12 of port D
+   RED_LED     : constant := 14; -- Red led is on pin 14
    period      : constant ada.real_time.time_span := ada.real_time.milliseconds (500);
 begin
 
-   -- Enable GPIOD periph clock
+   -- Enable gpiod periph clock
 
    interfaces.stm32.rcc.rcc_periph.ahb1enr.gpioden := 1;
 
-   -- Set GPIOD pins to output mode
+   -- Set gpiod pins to output mode
 
-   interfaces.stm32.gpio.gpiod_periph.moder.arr (GREEN_LED)
-      := system.stm32.mode_out;
-
-   interfaces.stm32.gpio.gpiod_periph.moder.arr (RED_LED)
-      := system.stm32.mode_out;
+   interfaces.stm32.gpio.gpiod_periph.moder.arr (GREEN_LED) := system.stm32.mode_out;
+   interfaces.stm32.gpio.gpiod_periph.moder.arr (RED_LED)   := system.stm32.mode_out;
 
    loop
       -- Led on / off
